b9f9eee39167a06ce108d298b6c3b171b25fea44
pipernash2004 <pipernashai999@gmail.com>
2025-12-16 08:42:45 +0200

    Revert "changes made again"
    This reverts commit 9cb747df8f2610d3680139660763b5a773a71389.

--- learningMS/views.py
+++ learningMS/views.py
@@ -25,7 +25,7 @@ from .serializers import (
     ReviewListSerializer, ReviewDetailSerializer, ReviewCreateSerializer,
     AchievementSerializer, CourseProgressSerializer, DashboardOverviewSerializer
 )
-from drf_spectacular.utils import extend_schema, OpenApiResponse
+from drf_spectacular.utils import extend_schema
 
 logger = logging.getLogger(__name__)
 
--- mysite/urls.py
+++ mysite/urls.py
@@ -28,6 +28,6 @@ urlpatterns = [
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema')),
 
     path('api/auth/', include('authentication.urls')),
-    path('api/checklist/', include('checklist.urls')),
+    # path('api/checklist/', include('checklist.urls')),
     path('api/lms/', include('learningMS.urls')),
 ]
